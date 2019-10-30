1c75d08506c4750e13063ce5d26c85e69aa83961
zubxir <zoomohammmed@gmail.com>
2019-10-29 22:24:12 -0500

    migrate db

--- Gemfile
+++ Gemfile
@@ -11,6 +11,7 @@ gem 'sqlite3', '~> 1.4'
 gem 'puma', '~> 3.11'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5'
+gem 'faker'
 # Transpile app-like JavaScript. Read more: https://github.com/rails/webpacker
 gem 'webpacker', '~> 4.0'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,68 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# frozen_string_literal: true
+
+Location.destroy_all
+Quote.destroy_all
+Spell.destroy_all
+Mastery.destroy_all
+JobClass.destroy_all
+Rank.destroy_all
+Champion.destroy_all
+
+# 13.times.do
+#     location = location.create(
+#       locationname: Faker::Games::LeagueOfLegends.unique.location
+#       jobclass: "Test"
+#     )
+# 5.times do
+#   Champion.create(
+#     champname: Faker::Games::LeagueOfLegends.unique.champion,
+#     location: Faker::Games::LeagueOfLegends.location,
+#     quote: Faker::Games::LeagueOfLegends.unique.quote,
+#     spell: Faker::Games::LeagueOfLegends.unique.summoner_spell,
+#     mastery: Faker::Games::LeagueOfLegends.unique.masteries,
+#     rank: Faker::Games::LeagueOfLegends.unique.rank
+#   )
+# end
+37.times do
+  Quote.create(
+    quotetext: Faker::Games::LeagueOfLegends.unique.quote
+  )
+end
+
+27.times do
+  Rank.create(
+    rank: Faker::Games::LeagueOfLegends.unique.rank
+  )
+end
+
+45.times do
+  Mastery.create(
+    masteryname: Faker::Games::LeagueOfLegends.unique.masteries
+  )
+end
+
+13.times do
+  Spell.create(
+    spellname: Faker::Games::LeagueOfLegends.unique.summoner_spell
+  )
+end
+
+4.times do
+  JobClass.create(
+    classname: Faker::Games::LeagueOfLegends.unique.champion
+  )
+end
+
+13.times do
+  Location.create(
+    locationname: Faker::Games::LeagueOfLegends.unique.location,
+    jobclassid: Faker::Number.within(range: 1..4)
+  )
+end
+
+puts "Generated #{Quote.count} quotes."
+puts "Generated #{Rank.count} ranks."
+puts "Generated #{Mastery.count} masteries."
+puts "Generated #{Spell.count} spells."
+puts "Generated #{JobClass.count} classes."
+# puts "Generated #{Location.count} locations."
